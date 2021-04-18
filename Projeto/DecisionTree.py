6ba7c224cd62c70fdfae6fb4c7f8600a2b1fbf03
João Rafael <jrsc2@cin.ufpe.br>
2021-04-18 19:58:58 -0300

    Decision Tree simples

---
+++ Projeto/DecisionTree.py
@@ -0,0 +1,64 @@
+import graphviz
+from IPython.display import SVG,display
+from sklearn import metrics
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+from StarsDataset import DatasetPanda, DatasetPandaColor, COL, CLASSIFICATION
+
+def make_decision_tree(data, features, classes):
+    decision_tree = DecisionTreeClassifier()
+    decision_tree.fit(data.train, data.train_classes)
+    decision_tree.score(data.test, data.test_classes)
+        
+    train_prediction = decision_tree.predict(data.train)
+    test_prediction = decision_tree.predict(data.test)
+    
+    print("\nResults for Training")
+    print(metrics.classification_report(data.train_classes, train_prediction))
+    
+    print("\nResults for Test")
+    print(metrics.classification_report(data.test_classes, test_prediction))
+    
+    print("\nFeature importance")
+    for feature, importance in zip(features, decision_tree.feature_importances_):
+        print("{}:{}".format(feature, importance))    
+    return decision_tree
+    
+def plot_tree(decision_tree, features, classes):
+    dot_data = export_graphviz( 
+         decision_tree, 
+         out_file=None,
+         feature_names=features,
+         class_names=classes,  
+         filled=True, 
+         rounded=True,
+         proportion=True,
+         node_ids=True,
+         rotate=False,
+         label='all',
+         special_characters=True
+        )
+    graph = graphviz.Source(dot_data)  
+    display(SVG(graph.pipe(format='svg')))
+    
+def run_for_type():
+    data = DatasetPanda(drop_columns=["Color", "Spectral_Class"])
+    # print(data.original)
+    features = [*COL.keys()][:4]
+    classes = [*CLASSIFICATION.values()][:6]
+    tree = make_decision_tree(data, features, classes)
+    plot_tree(tree, features, classes)
+    
+def run_for_color():
+    data = DatasetPandaColor(drop_columns=["Color", "Spectral_Class"])
+    # print(data.original)
+    features = [*COL.keys()][:5]
+    classes = None
+    make_decision_tree(data, features, classes)
+    tree = make_decision_tree(data, features, classes)
+    plot_tree(tree, features, classes)
+
+run_for_type()
+    
--- Projeto/StarsDataset.py
+++ Projeto/StarsDataset.py
@@ -31,36 +31,69 @@ class Dataset(object):
     def __init__(self):
         pass
     
-    def drop_if_necessary(self, drop_columns):
+    def drop(self, drop_columns):
         if drop_columns:
             self.original = self.original.drop(columns=drop_columns)
 
 class DatasetNumpy(Dataset):
     def __init__(self, drop_columns=None):
         self.original = pd.read_csv(FILENAME)
-        self.drop_if_necessary(drop_columns)
+        self.drop(drop_columns)
         self.original = self.original.to_numpy()
         self.train, self.test, self.train_classes, self.test_classes = train_test_split(
             np.array([i[:COL["Classification"]] for i in self.original]), 
             np.array([i[COL["Classification"]] for i in self.original]), 
             train_size=TRAIN_TO_TEST_RATIO, 
             stratify=np.array([i[COL["Classification"]] for i in self.original]))
+        _, _, self.train_colors, self.test_colors = train_test_split(
+            np.array([i[:COL["Color"]] for i in self.original]), 
+            np.array([i[COL["Color"]] for i in self.original]), 
+            train_size=TRAIN_TO_TEST_RATIO, 
+            stratify=np.array([i[COL["Color"]] for i in self.original]))
 
         
 
 class DatasetPanda(Dataset):    
     def __init__(self, drop_columns=None):
         self.original = pd.read_csv(FILENAME)
-        self.drop_if_necessary(drop_columns)
+        self.normalize_color()
+        self.classes = self.original["Type"]
+        self.drop("Type")
+        self.drop(drop_columns)
         self.train, self.test, self.train_classes, self.test_classes = train_test_split(
             self.original, 
-            self.original["Type"], 
+            self.classes, 
             train_size=TRAIN_TO_TEST_RATIO, 
-            stratify=self.original["Type"])
+            stratify=self.classes)
+        self.scale()
+
+    def scale(self):
         scaler = StandardScaler()
         scaler.fit(self.train)
         self.scaled_train = scaler.transform(self.train)
         self.scaled_test = scaler.transform(self.test)
+        
+    def normalize_color(self):
+        self.original["Color"] = self.original["Color"].str.lower()
+        self.original["Color"] = self.original["Color"].str.replace(" ", "-")
+        self.original["Color"] = self.original["Color"].str.replace("whitish", "white")
+        self.original["Color"] = self.original["Color"].str.replace("ish", "")
+        self.original["Color"] = self.original["Color"].str.replace("orange-red", "orange")
+        self.original["Color"] = self.original["Color"].str.replace("pale-yellow-orange", "orange")
+        self.original["Color"] = self.original["Color"].str.replace("white-yellow", "yellow")
+        
+class DatasetPandaColor(DatasetPanda):
+    def __init__(self, drop_columns=None):
+        self.original = pd.read_csv(FILENAME)
+        self.normalize_color()
+        colors = self.original["Color"]
+        self.drop(drop_columns)
+        self.train, self.test, self.train_classes, self.test_classes = train_test_split(
+            self.original,
+            colors,
+            train_size=TRAIN_TO_TEST_RATIO,
+            stratify=colors)
+        self.scale()
         
         
 def print_star(star, star_classification=None):
